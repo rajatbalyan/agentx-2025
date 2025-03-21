4e0fe3afcc4818aa7a2f73f2c442420f0fe41059
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2025-03-21 18:02:17 +0530

    (feat) auditor added

---
+++ agentx/auditor/lighthouse-config.js
@@ -0,0 +1,29 @@
+module.exports=    {
+    settings: {   
+        onlyCategories: ['performance', 'seo'],
+        onlyAudits: [
+            "first-contentful-paint",
+            "last-contentful-paint",
+            "first-meaningful-paint",
+            "largest-contentful-paint-element",
+            "speed-index",
+            "total-blocking-time",
+            "server-response-time.",
+            "interactive",
+            "bootup-time",
+            "long-tasks",
+            "unused-images",
+            "render-blocking-resources",
+            "unminified-css",
+            "unminified-jaavscript",
+            "unused-css-rules",
+            "unused-javascript",
+            "uses-responsive-images",
+            "meta-description",
+            "link-text",
+            "robots.txt",
+            "render-blocking-insight",
+            "image-alt",
+        ]
+    }
+};
